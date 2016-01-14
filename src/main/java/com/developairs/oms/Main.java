5c037e9ef581d703496e50f35cb725de3c2a2b9c
Alireza19330 <alireza19330@gmail.com>
2016-01-14 15:49:46 +0330

    intial commit

---
+++ src/main/java/com/developairs/oms/Main.java
@@ -0,0 +1,34 @@
+package com.developairs.oms;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		/*
+		
+		// Create a configSpring EmployeeServiceuration instance
+        Configuration configuration = new Configuration();
+        // Provide configuration file
+        configuration.configure("hibernate.cfg.xml");
+        // Build a SessionFactory
+        SessionFactory factory = configuration.buildSessionFactory(new StandardServiceRegistryBuilder().configure().build());
+        // Get current session, current session is already associated with Thread
+        Session session = factory.getCurrentSession();
+        // Begin transaction, if you would like save your instances, your calling of save must be associated with a transaction
+        session.getTransaction().begin();
+        // Create employee
+        Employee emp = new Employee();
+        emp.setEmployeeName("Peter Jousha");
+        emp.setEmployeeSalary(2000);
+        emp.setEmployeeHireDate(new Date());
+        // Save
+        session.save(emp);
+        // Commit, calling of commit will cause save an instance of employee
+        session.getTransaction().commit();
+        
+        */
+	}
+	
+}
